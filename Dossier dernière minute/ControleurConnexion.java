c5046692aed16a431cee2a9cfd8651e5d038e4e9
WillGrab <william.grabie@gmail.cim>
2014-01-16 21:33:34 +0100

    Update ControleurConnexion.java

--- Dossier dernière minute/ControleurConnexion.java
+++ Dossier dernière minute/ControleurConnexion.java
@@ -6,13 +6,27 @@ import javax.swing.JOptionPane;
 import vue.*;
 import modele.*;
 
+/**  
+ * Controleur de la vue connexion
+ * @author Groupe G24
+ */
 public class ControleurConnexion {
 	private ControleurDonneesUtilisateur controleurDonneesU;
 	
+	/**
+	 * Constructeur<br>
+	 * Reçoit les données utilisateur.
+	 * @param _cdu
+	 */
 	public ControleurConnexion(ControleurDonneesUtilisateur _cdu) {
 		this.controleurDonneesU = _cdu;
 	}
 	
+	/**
+	 * Permet de récupérer l'identifiant de l'utilisateur.
+	 * @param _vc
+	 * @return String
+	 */
 	public String getChampIdUtilisateur(VueConnexion _vc){
 		String champId;
 		champId = _vc.getChampIdUtilisateur().getText();
@@ -25,13 +39,22 @@ public class ControleurConnexion {
 		}
 	}
 		
-		public String getChampMdp(VueConnexion _vc){
-			char[] mdp = _vc.getChampMotDePasseUtilisateur().getPassword();
-		    String Mdp = new String(mdp);
-		    
-			return(Mdp);
-		}
-	
+	/**
+	 * Permet de récupérer le mot de passe de l'utilisateur.
+	 * @param _vc
+	 * @return String
+	 */
+	public String getChampMdp(VueConnexion _vc){
+		char[] mdp = _vc.getChampMotDePasseUtilisateur().getPassword();
+	    String Mdp = new String(mdp);
+	    
+		return(Mdp);
+	}
+	/**
+	 * Validation de l'identifiant de l'utilisateur.
+	 * @param _vc
+	 * @return Boolean
+	 */
 	public boolean controleurValiderIdentifiant(VueConnexion _vc) {
 		boolean identificationCorrect = false;
 		Utilisateur user;
